69c9d38ca513332ffb7a26204b4bf89d5799d21e
Hasan Rashed <hasanrashed537@gmail.com>
2021-08-31 22:14:16 +0600

    jdbd MySQL Added

--- jdbcMysqlDemo/src/JDBCDemo.java
+++ jdbcMysqlDemo/src/JDBCDemo.java
@@ -2,8 +2,8 @@ import java.sql.*;
 
 public class JDBCDemo
 {
-    public static void main(String[] args)
-    {
+public static void main(String[] args)
+{
         String url = "jdbc:mysql://localhost:3306/jdbcdemo";
         String username = "root";
         String password = "";
